9b3ca484112177804cdb5ad2bd2e6361cf8c6c9f
rifqiay <muhammadrifqiay5@gmail.com>
2022-10-18 13:37:50 +0700

    delete package tailwind and use tailwind cdn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import './index.css';
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -20,13 +19,16 @@ import "react-toastify/dist/ReactToastify.css";
 
 import { Provider } from "react-redux";
 import store from "./app/redux/store";
+import { HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </BrowserRouter>
   </Provider>
   // </React.StrictMode>
