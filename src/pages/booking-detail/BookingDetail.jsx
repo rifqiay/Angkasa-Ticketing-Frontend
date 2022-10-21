c312d37395503318982f1a119c512ea13b34c72b
bagus25dzikri06 <bagusdzikri@gmail.com>
2022-10-21 20:35:27 +0700

    Update reslicing Booking Detail

--- src/pages/booking-detail/BookingDetail.jsx
+++ src/pages/booking-detail/BookingDetail.jsx
@@ -1,67 +1,38 @@
-import React from "react";
+import React, { Fragment } from "react";
+import styles from "./bookingDetail.module.css"
 import { BsThreeDotsVertical } from 'react-icons/bs';
-import { Helmet } from "react-helmet-async";
 
 const BookingDetail = () => {
-  return <>
-    <Helmet>
-      <script src="https://cdn.tailwindcss.com"></script>
-    </Helmet>
-
-    <div className="overflow-hidden bg-blue-600">
-      <div className="col-12 my-28">
-        <div className="py-5 mx-[200px] text-center bg-white rounded">
-          <div className="mx-20">
-            <div className="row">
-              <div className="my-8 text-4xl font-bold col-6 text-start">Booking Pass</div>
-              <div className="flex justify-end py-2 col-6"><BsThreeDotsVertical size={30} /></div>
+  return <Fragment>
+    <div className={styles.bookingdetail_background}>
+      <div className='container'>
+        <div className={styles.bookingdetail_sect1}>
+          <div className='row'>
+            <div className='col-6 text-start'><h1><b>Booking Pass</b></h1></div>
+            <div className='col-6'>
+              <BsThreeDotsVertical size={30} className='float-end' style={{
+                margin: '7px 0'
+              }} />
             </div>
-            <div className="my-6 border divide-x rounded-lg row divide-dashed">
-              <div className="p-5 col-8">
-                <div className='row'>
-                  <div className='py-2 col-3'>
-                    <img src="/images/garuda-indonesia-logo-BD82882F07-seeklogo3.svg" width={150} height={127} />
-                  </div>
-                  <div className='py-4 text-4xl font-bold text-center col-3'>IDN</div>
-                  <div className='py-4 col-2'>
-                    <img src='/images/Vector.svg' width={38} height={37} />
-                  </div>
-                  <div className='py-4 text-4xl font-bold text-start col-4'>JPN</div>
-                </div>
-                <div className='pb-3 mt-8'>
-                  <div className='text-gray-400 row text-start'>
-                    <div className='col-6'>Code</div>
-                    <div className='col-6'>Class</div>
-                  </div>
-                  <div className='row text-start'>
-                    <div className='text-lg col-6'>AB-221</div>
-                    <div className='text-lg col-6'>Economy</div>
-                  </div>
-                </div>
-                <div className='py-3'>
-                  <div className='text-gray-400 row text-start'>
-                    <div className='col-6'>Terminal</div>
-                    <div className='col-6'>Gate</div>
-                  </div>
-                  <div className='row text-start'>
-                    <div className='text-lg col-6'>A</div>
-                    <div className='text-lg col-6'>221</div>
-                  </div>
-                </div>
-                <div className='py-3 text-start'>
-                  <div className='text-gray-400 col-12'>Departure</div>
-                  <div className='text-lg col-12'>Monday, 20 July ‘20 - 12:33</div>
-                </div>
-              </div>
-              <div className="col-4 py-10">
-                <img src="/images/Group1126.svg" className="mx-auto d-block" width={135} height={72} />
-              </div>
+          </div>
+          <div className={styles.bookingdetail_sect2}>
+            <div className='row'>
+              <div className='col-8' style={{
+                borderStyle: 'solid dashed solid solid',
+                borderColor: '#E5E5E5',
+                borderRadius: '10px'
+              }}>1</div>
+              <div className='col-4' style={{
+                borderStyle: 'solid solid solid none',
+                borderColor: '#E5E5E5',
+                borderRadius: '10px'
+              }}>2</div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </>;
+  </Fragment>
 };
 
 export default BookingDetail;
