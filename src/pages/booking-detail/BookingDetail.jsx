ebf1d1e5e23b0bad6cd2af4a875732f38f2e8cc9
Rifqi Ahmad Pratama <rifqiahmad234a@gmail.com>
2022-10-21 14:19:00 +0700

    Slicing Booking Detail

--- src/pages/booking-detail/BookingDetail.jsx
+++ src/pages/booking-detail/BookingDetail.jsx
@@ -1,23 +1,68 @@
-import React from "react";
-import { BsThreeDotsVertical } from 'react-icons/bs'
-
+import React, { Fragment } from "react";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import ImageGaruda from "../../assets/images/booking/garuda.png";
+import { QRCodeSVG } from "qrcode.react";
 const BookingDetail = () => {
-  return <div className="overflow-hidden bg-blue-600">
-    <div className="col-12 my-28">
-      <div className="py-3 mx-[200px] text-center bg-white rounded">
-        <div className="mx-20">
+  return (
+    <Fragment>
+      <div className="BookingDetail-Page bg-primary ">
+        <div className="container justify-content-center ">
           <div className="row">
-            <div className="col-6 text-start text-4xl font-bold">Booking Pass</div>
-            <div className="col-6 flex justify-end py-2"><BsThreeDotsVertical size={30} /></div>
-          </div>
-          <div className="row border divide-x divide-dashed my-6">
-            <div className="col-6">1</div>
-            <div className="col-6">2</div>
+            <div className="col-lg-10 offset-md-1">
+              <div className="card mt-5 mb-5">
+                <div className="card-header bg-white">
+                  <div className="row justify-content-between mt-2">
+                    <div className="col-4">
+                      <h2 className="card-title">Booking Pass</h2>
+                    </div>
+                    <div className="col-1">
+                      <BsThreeDotsVertical size={30} />
+                    </div>
+                  </div>
+                </div>
+                <div className="container">
+                  <div className="card-body mt-3 ">
+                    <div className="row">
+                      <div className="col-lg-6">
+                        <img src={ImageGaruda} alt="ImageGaruda" />
+                        <div class="row justify-content-evenly mt-5">
+                          <div class="col-4">
+                            <p>Code</p>
+                            <p>AB-221</p>
+                          </div>
+                          <div class="col-6">
+                            <p>Class</p>
+                            <p>Economy</p>
+                          </div>
+                        </div>
+                        <div class="row justify-content-evenly mt-5">
+                          <div class="col-4">
+                            <p>Terminal</p>
+                            <p>A</p>
+                          </div>
+                          <div class="col-6">
+                            <p>Gate</p>
+                            <p>221</p>
+                          </div>
+                        </div>
+                        <div className="container">
+                          <p>Departure</p>
+                          <p>Monday, 20 July'20 - 12:33</p>
+                        </div>
+                      </div>
+                      <div className="col-4 text-center justify-content-center">
+                        <QRCodeSVG value="https://reactjs.org/" size={250} />,
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>;
+    </Fragment>
+  );
 };
 
 export default BookingDetail;
